9ec4bc7c7863640a006ed95d5b8c76a22dde0365
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-08-12 15:33:25 +0530

    Car Dealership project, toString() and equals() methods.

---
+++ car_dealership/src/car_dealership/Customer.java
@@ -0,0 +1,37 @@
+package car_dealership;
+
+public class Customer {
+	
+	private String name;
+	private String address;
+	private double cashOnHand;
+	
+	public void purchaseCar(Vehicle vehicle, Employee emp, boolean finance) {
+		emp.handleCustomer(this, finance, vehicle);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getCashOnHand() {
+		return cashOnHand;
+	}
+
+	public void setCashOnHand(double cashOnHand) {
+		this.cashOnHand = cashOnHand;
+	}
+	
+}
