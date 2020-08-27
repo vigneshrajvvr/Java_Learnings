c3ce4bc0b356004fb2f52db78439af7561400491
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-08-27 23:34:23 +0530

    Regular expressions introduction

--- RegularExpressions/src/com/expressions/practice/Main.java
+++ RegularExpressions/src/com/expressions/practice/Main.java
@@ -9,8 +9,25 @@ public class Main {
 		String yourString = string.replaceAll("I", "You");
 		System.out.println(yourString);
 		
-		String alphanumeric = "abcDeeeF12Ghhiiiijkl9z";
+		String alphanumeric = "abcDeeeF12Ghhiiiijkl99z";
 		System.out.println(alphanumeric.replaceAll(".", "Y"));
+		
+		// ^ to check for start of the string
+		System.out.println(alphanumeric.replaceAll("^abcDeee", "YYY"));
+		
+		String secondString = "abcDeeeF12GhhabcDeeeiiiijkl99z";
+		System.out.println(secondString.replaceAll("^abcDeee", "YYY"));
+		
+		System.out.println(alphanumeric.matches("^hello"));
+		
+		//In matches method, usage of ^ will check for whole string
+		System.out.println(alphanumeric.matches("^abcDeee"));
+		System.out.println(alphanumeric.matches("^abcDeeeF12Ghhiiiijkl99z"));
+		
+		System.out.println(alphanumeric.replaceAll("ijkl99z$", "THE END"));
+		System.out.println(alphanumeric.replaceAll("[aei]", "X"));
+		System.out.println(alphanumeric.replaceAll("[aei]", "I replaced a letter here"));
+		System.out.println(alphanumeric.replaceAll("[aei][Fj]", "X"));
+		
 	}
-
 }
