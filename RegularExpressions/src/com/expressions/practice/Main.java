407b1f3159f09a46fb8b9c60203e647db2d32ef5
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-08-27 22:56:30 +0530

    Regular expressions introduction

---
+++ RegularExpressions/src/com/expressions/practice/Main.java
@@ -0,0 +1,16 @@
+package com.expressions.practice;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String string = "I am a string. Yes, I am.";
+		System.out.println(string);
+		String yourString = string.replaceAll("I", "You");
+		System.out.println(yourString);
+		
+		String alphanumeric = "abcDeeeF12Ghhiiiijkl9z";
+		System.out.println(alphanumeric.replaceAll(".", "Y"));
+	}
+
+}
