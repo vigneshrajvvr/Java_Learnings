8f7aa733e45eb424ab19072a474c77da5bba45a8
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-09-14 22:37:23 +0530

    Decorator Pattern

---
+++ Design_Pattern/src/decorator_pattern/Beverage.java
@@ -0,0 +1,27 @@
+package decorator_pattern;
+
+public abstract class Beverage {
+
+	public enum Size {
+		TALL, GRANDE, VENTI
+	};
+
+	Size size = Size.TALL;
+
+	String description = "Unknown Beverage";
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Size getSize() {
+		return size;
+	}
+
+	public void setSize(Size size) {
+		this.size = size;
+	}
+
+	public abstract double cost();
+
+}
---
+++ Design_Pattern/src/decorator_pattern/HouseBlend.java
@@ -0,0 +1,14 @@
+package decorator_pattern;
+
+public class HouseBlend extends Beverage {
+	
+	public HouseBlend() {
+		this.description = "House Blend Coffee";
+	}
+
+	@Override
+	public double cost() {
+		return 0.89;
+	}
+
+}
