e5019095a8202939099f08e969045da232639b13
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-02-21 23:03:37 +0530

    toString() and equals() methods

--- Advanced_Class_Design/src/com/practice/vvr/java8/ObjectMethods.java
+++ Advanced_Class_Design/src/com/practice/vvr/java8/ObjectMethods.java
@@ -2,8 +2,17 @@ package com.practice.vvr.java8;
 
 public class ObjectMethods {
 	public static void main(String args[]) {
+		// toString()
 		Hippo1 h1 = new Hippo1("Harry", 3100);
 		System.out.println(h1); //Hippo1 [name=Harry, weight=3100.0]
+		
+		// equals()
+		String s1 = new String("lion");
+		String s2 = new String("lion");
+		System.out.println(s1.equals(s2));                  // true
+		StringBuilder sb1 = new StringBuilder("lion");
+		StringBuilder sb2 = new StringBuilder("lion");
+		System.out.println(sb1.equals(sb2));                // false
 	}
 }
 
@@ -26,5 +35,53 @@ class Hippo1 {
 	public String toString() {
 		return "Hippo1 [name=" + name + ", weight=" + weight + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(weight);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	
+
+	
+//	<dependency>
+//    <groupId>commons-lang</groupId>
+//    <artifactId>commons-lang</artifactId>
+//    <version>2.5</version>
+//    </dependency>
+	
+//	 @Override public String toString() {
+//		   return ToStringBuilder.reflectionToString(this,
+//		      ToStringStyle.SHORT_PREFIX_STYLE);
+//		}
+	
+//	output: Hippo[name=Harry,weight=3100.0]
+	
+// --------------------------------------------------------------
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hippo1 other = (Hippo1) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(weight) != Double.doubleToLongBits(other.weight))
+			return false;
+		return true;
+	}
+	
 	
 }
