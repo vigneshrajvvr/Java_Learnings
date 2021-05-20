e99ee767685c617b5d6e78360038dc6e98031950
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-05-21 02:32:30 +0530

    Interface variables and default interface methods

--- Advanced_Class_Design/src/com/practice/vvr/java8/ExtendedInterface.java
+++ Advanced_Class_Design/src/com/practice/vvr/java8/ExtendedInterface.java
@@ -1,14 +1,24 @@
 package com.practice.vvr.java8;
 
 public interface ExtendedInterface {
+
 	public abstract int getTailLength();
+
+	public static final int LENGTH = 10; // Interface variable
+	/*
+	 * Default method - Class which implements the interface is not required to
+	 * implement the default method
+	 */
+
+	public default double getTemperature() {
+		return 10.0;
+	}
 }
 
 /*
- * An interface can extend any number of interfaces but
- * the concrete class which implements the interface
- * should implement all the abstract methods including the
- * abstract methods of inherited interface
+ * An interface can extend any number of interfaces but the concrete class which
+ * implements the interface should implement all the abstract methods including
+ * the abstract methods of inherited interface
  */
 interface HashWiskers extends ExtendedInterface {
 	public abstract int getNumberOfWishkers();
@@ -18,14 +28,12 @@ class Extended implements HashWiskers {
 
 	@Override
 	public int getTailLength() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int getNumberOfWishkers() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
-	
+
 }
