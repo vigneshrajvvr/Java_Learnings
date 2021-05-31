3f32f1637f1e30f199fee9439be23feaf5ba8303
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-06-01 00:08:09 +0530

    Default methods in interface

--- Advanced_Class_Design/src/com/practice/vvr/java8/InterfaceRecap.java
+++ Advanced_Class_Design/src/com/practice/vvr/java8/InterfaceRecap.java
@@ -13,12 +13,31 @@ public interface InterfaceRecap {
 	// public abstract int getMaximumDepth();
 	int getMaximumDepth();
 	
+	/*
+	 * Default method with implementation
+	 * Class which implements the interface may or may not override default method
+	 * This is to add new functionality without breaking the existing functionalities
+	 * Cannot to be static, final, or abstract
+	 */
+	public default double getTemperature() {
+		return 20.0;
+	}
+	
 }
 
 /*
  * Class which uses this interface will have "implements" keyword
  * 
- * Extending mulitple interfaces, example:
+ * Extending multiple interfaces, example:
  * public class Elephant implements WalksOnFourLegs, HasTrunk, Herbivore {}
  * 
  */
+
+class ClassInterfaceRecap implements InterfaceRecap{
+
+	@Override
+	public int getMaximumDepth() {
+		return 0;
+	}
+	
+}
