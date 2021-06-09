3d99df09e0473057b536ee16f30b7b106b45c674
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-06-10 01:05:36 +0530

    Functional Interface introduction

---
+++ Advanced_Class_Design/src/com/practice/vvr/java8/FunctionalInterfaceExample.java
@@ -0,0 +1,20 @@
+package com.practice.vvr.java8;
+
+/*
+ * Functional interface is an interface which contain
+ * only one abstract method
+ * 
+ * Marking these kind of interfaces with @FunctionInterface
+ * annotation is a good practice.
+ * 
+ * If we mark the interface with @FunctionInterface annotation
+ * then if there are more the one abstract methods then 
+ * error shows
+ */
+
+@FunctionalInterface
+public interface FunctionalInterfaceExample {
+	
+	public abstract void sprint();
+	
+}
