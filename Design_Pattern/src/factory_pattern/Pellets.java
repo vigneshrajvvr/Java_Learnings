272a7c4772235e99188b93ef0534ddc59e4f9033
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-07-15 23:24:09 +0530

    Factory pattern

---
+++ Design_Pattern/src/factory_pattern/Pellets.java
@@ -0,0 +1,14 @@
+package factory_pattern;
+
+public class Pellets extends Food {
+
+	public Pellets(int quantity) {
+		super(quantity);
+	}
+
+	@Override
+	public void consumed() {
+		System.out.println("Pellets eaten: "+getQuantity());
+	}
+
+}
