2a29e9b96d915cd78ca9a433a44c94cc8dbdb697
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-12-23 23:40:43 +0530

    Access Modifiers recap

--- Advanced_Class_Design/src/com/practice/vvr/java8/FunctionalInterfaceExample.java
+++ Advanced_Class_Design/src/com/practice/vvr/java8/FunctionalInterfaceExample.java
@@ -3,27 +3,27 @@ package com.practice.vvr.java8;
 /*
  * Functional interface is an interface which contain
  * only one abstract method
- * 
+ *
  * Marking these kind of interfaces with @FunctionInterface
  * annotation is a good practice.
- * 
+ *
  * If we mark the interface with @FunctionInterface annotation
- * then if there are more the one abstract methods then 
+ * then if there are more the one abstract methods then
  * error shows
  */
 
 @FunctionalInterface
 public interface FunctionalInterfaceExample {
-	
-	public abstract void sprint();
-	
+
+    public abstract void sprint();
+
 }
 
 class Example1 implements FunctionalInterfaceExample {
 
-	@Override
-	public void sprint() {
-		System.out.println("Implementing functional interface method");
-	}
-	
+    @Override
+    public void sprint() {
+        System.out.println("Implementing functional interface method");
+    }
+
 }
