0815b3862e158d8a97b3bb0a55a4d331ebd528b8
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-03-01 00:20:12 +0530

    equals() and hashcode() methods

--- OCP_Preparation/src/advanced_class_design_01/ObjectMethods.java
+++ OCP_Preparation/src/advanced_class_design_01/ObjectMethods.java
@@ -1,5 +1,7 @@
 package advanced_class_design_01;
 
+import java.util.Objects;
+
 public class ObjectMethods {
 
     private String objectMethodName;
@@ -16,9 +18,27 @@ public class ObjectMethods {
         System.out.println(obj1.toString());
     }
 
+    // overridden from Object class
     @Override
     public String toString() {
         return this.objectMethodName + " " + this.objectMethodNumber;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ObjectMethods)) return false;
+        ObjectMethods that = (ObjectMethods) o;
+        return objectMethodNumber == that.objectMethodNumber && Objects.equals(objectMethodName, that.objectMethodName);
+    }
+
+    /*
+        - Hashcode must not change
+        - if equals method return true then hashcode method also should return true
+        - if equals method return false then the results need not be necessarily be unique
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(objectMethodName, objectMethodNumber);
+    }
 }
