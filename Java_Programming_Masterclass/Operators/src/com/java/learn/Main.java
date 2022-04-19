e7ffa3fad3cbfa40681b08faa0fa595920bea2e3
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-04-19 12:49:01 +0530

    ternary operator and operator precedence

--- Java_Programming_Masterclass/Operators/src/com/java/learn/Main.java
+++ Java_Programming_Masterclass/Operators/src/com/java/learn/Main.java
@@ -70,8 +70,29 @@ public class Main {
         }
 
         boolean isCar = false;
-        if(isCar != true) {
+        if(isCar) {
             System.out.println("This is not supposed to happen");
         }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if(wasCar) {
+            System.out.println("wasCar is true");
+        }
+
+        // Operator challenge
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+        System.out.println("myValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+        boolean isNoRemainder = theRemainder == 0 ? true : false;
+        System.out.println("isNoRemainder : " + isNoRemainder);
+
+        if(!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
+
     }
 }
