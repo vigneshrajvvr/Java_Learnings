7958ffd967922b509dde2bf754dca611d136ffb5
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-04-19 12:05:02 +0530

    if statement and logical operators

--- Java_Programming_Masterclass/Operators/src/com/java/learn/Main.java
+++ Java_Programming_Masterclass/Operators/src/com/java/learn/Main.java
@@ -43,5 +43,35 @@ public class Main {
         //result = result - 2
         result -= 2; // 10 - 2 = 8
         System.out.println("10 - 2 =  " + result);
+
+        boolean isAlien = false;
+        if(isAlien == false) {
+            System.out.println("It is not an alien");
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topScore = 100;
+        if(topScore == 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if((topScore > secondTopScore) && (topScore <= 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if(newValue == 50) {
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if(isCar != true) {
+            System.out.println("This is not supposed to happen");
+        }
     }
 }
