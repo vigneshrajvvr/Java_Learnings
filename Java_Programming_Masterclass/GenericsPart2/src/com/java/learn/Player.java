f3ab727534885f17289726b05da8989457692c8a
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-04-20 23:23:18 +0530

    generics usage in custom classes

---
+++ Java_Programming_Masterclass/GenericsPart2/src/com/java/learn/Player.java
@@ -0,0 +1,14 @@
+package com.java.learn;
+
+public abstract class Player {
+
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ Java_Programming_Masterclass/GenericsPart2/src/com/java/learn/SoccerPlayer.java
@@ -0,0 +1,9 @@
+package com.java.learn;
+
+public class SoccerPlayer extends Player{
+
+    public SoccerPlayer(String name) {
+        super(name);
+    }
+
+}
