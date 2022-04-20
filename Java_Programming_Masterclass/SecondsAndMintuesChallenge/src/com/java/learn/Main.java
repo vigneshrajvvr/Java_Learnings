df6f171bb4580b290fade18f0b41c08a1e6f7a80
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-04-20 22:05:22 +0530

    Seconds and minutes challenge

--- Java_Programming_Masterclass/SecondsAndMintuesChallenge/src/com/java/learn/Main.java
+++ Java_Programming_Masterclass/SecondsAndMintuesChallenge/src/com/java/learn/Main.java
@@ -2,28 +2,46 @@ package com.java.learn;
 
 public class Main {
 
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
+
     public static void main(String[] args) {
         System.out.println(getDurationString(65, 45));
         System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-41));
 
     }
 
     public static String getDurationString(int minutes, int seconds) {
 
         if(minutes < 0 || seconds < 0 || seconds > 59) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         int hours = minutes / 60;
         int remainingMinutes = minutes % 60;
 
-        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+        String hoursString = hours + "h";
+        if(hours < 10) {
+            hoursString = "0" + hoursString;
+        }
+
+        String minuteString = remainingMinutes + "m";
+        if(remainingMinutes < 10) {
+            minuteString = "0" + minuteString;
+        }
+
+        String secondsString = seconds + "s";
+        if(seconds < 10) {
+            secondsString = "0" + secondsString;
+        }
+
+        return hoursString + " " + minuteString + " " + secondsString;
 
     }
 
     public static String getDurationString(int seconds) {
         if(seconds < 0) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         int minutes = seconds / 60;
