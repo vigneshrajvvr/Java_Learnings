bdccfc99f7f1983d5235ced7863780e2b8e7fbf9
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-12-27 17:57:08 +0530

    Cyclic Barriers to order the tasks

---
+++ Java 8_11/Concurrency/src/CyclicBarrierExample.java
@@ -0,0 +1,50 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CyclicBarrierExample {
+	
+	public void removeLions() {
+		System.out.println("Removing lions");
+	}
+	
+	public void cleanPen() {
+		System.out.println("Cleaning the pen");
+	}
+	
+	public void addLions() {
+		System.out.println("Adding lions");
+	}
+	
+	public void performTask(CyclicBarrier wait1, CyclicBarrier wait2) {
+		
+		try {
+			removeLions();
+			wait1.await();
+			cleanPen();
+			wait2.await();
+			addLions();
+		} catch (InterruptedException | BrokenBarrierException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String args[]) {
+		ExecutorService executor = null;
+		var manager = new CyclicBarrierExample();
+		try {
+			executor = Executors.newFixedThreadPool(4);
+			var wait1 = new CyclicBarrier(4);
+			var wait2 = new CyclicBarrier(4, () -> System.out.println("*** Pen Cleaned!"));
+			for(int i = 0; i < 4; i++) {
+				executor.submit(() -> manager.performTask(wait1, wait2));
+			}
+		} finally {
+			if(executor != null) {
+				executor.shutdown();
+			}
+		}
+	}
+
+}
--- Java 8_11/Concurrency/src/LockFramework.java
+++ Java 8_11/Concurrency/src/LockFramework.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -8,14 +9,19 @@ public class LockFramework {
 		Lock lock = new ReentrantLock();
 		new Thread(() -> printMessage(lock)).start();
 		
-		if(lock.tryLock()) {
-			try {
-				System.out.println("Lock obtained, entering protected code");
-			} finally {
-				lock.unlock();
+//		if(lock.tryLock()) {
+		try {
+			if(lock.tryLock(10, TimeUnit.SECONDS)) {
+				try {
+					System.out.println("Lock obtained, entering protected code");
+				} finally {
+					lock.unlock();
+				}
+			} else {
+				System.out.println("Unable to acquire lock, doing something else");
 			}
-		} else {
-			System.out.println("Unable to acquire lock, doing something else");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 		
 	}
