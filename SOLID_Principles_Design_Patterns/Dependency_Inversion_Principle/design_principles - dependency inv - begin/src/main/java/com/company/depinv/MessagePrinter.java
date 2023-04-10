452bb0fe0840bf92ef984a2ab2779af27c738d15
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2023-04-10 17:26:44 +0530

    Dependency Inversion Principle

---
+++ SOLID_Principles_Design_Patterns/Dependency_Inversion_Principle/design_principles - dependency inv - begin/src/main/java/com/company/depinv/MessagePrinter.java
@@ -0,0 +1,13 @@
+package com.company.depinv;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MessagePrinter {
+
+	// Writes message to a file
+	public void writeMessage(Message msg, PrintWriter writer, Formatter formatter) throws IOException {
+		writer.println(formatter.format(msg)); // formats and writes message
+		writer.flush();
+	}
+}
