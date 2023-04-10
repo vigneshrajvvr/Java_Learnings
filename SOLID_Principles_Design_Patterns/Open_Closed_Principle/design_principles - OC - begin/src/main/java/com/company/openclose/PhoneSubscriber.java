9524994a0d89ea2228f7e545d40fe3debb3fe4fb
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2023-04-10 15:58:53 +0530

    Open Closed Principle

---
+++ SOLID_Principles_Design_Patterns/Open_Closed_Principle/design_principles - OC - begin/src/main/java/com/company/openclose/PhoneSubscriber.java
@@ -0,0 +1,14 @@
+package com.company.openclose;
+
+import java.util.List;
+
+public class PhoneSubscriber extends Subscriber{
+
+	@Override
+    public double calculateBill() {
+        List<CallHistory.Call> sessions = CallHistory.getCurrentCalls(subscriberId);
+        long totalDuration = sessions.stream().mapToLong(CallHistory.Call::getDuration).sum();
+        return totalDuration*baseRate/100;
+    }
+
+}
