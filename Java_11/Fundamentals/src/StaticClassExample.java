c6024876ebb9ed8849f0f6900ff15ecdbd9c2c64
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2023-11-15 00:00:55 +0530

    Static Inner Classes

---
+++ Java_11/Fundamentals/src/StaticClassExample.java
@@ -0,0 +1,33 @@
+
+/*
+The Learn Programming Academy
+Java SE 11 Developer 1Z0-819 OCP Course - Part 2
+Section 1: Java Fundamentals
+Topic 2: Nested Classes
+Subtopic: Static Nested Classes
+*/
+
+
+import nest.EnclosingClass;
+import nest.EnclosingClass.NestedStaticClass;
+
+public class StaticClassExample {
+    public static void main(String[] args) {
+        // Reference static field on static nested class directly:
+        System.out.println(NestedStaticClass.staticName);
+
+        // Reference static method on static nested class directly:
+        System.out.println(NestedStaticClass.getStaticName());
+
+        // Reference enum on static nested class directly:
+        System.out.println(NestedStaticClass.Colors.BLUE);
+
+        // Local variable declaration using a nested class's interface
+        NestedStaticClass.NestedInterface n;
+
+        // Create an instance of the static class
+        EnclosingClass.NestedStaticClass nInstance = new EnclosingClass.NestedStaticClass();
+        System.out.println(nInstance.getInstanceName());
+
+    }
+}
